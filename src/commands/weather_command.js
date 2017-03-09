867cf9c8485846b82f1dc8c4f92891c8c29a6539
Daniel Tam <ctam.daniel@gmail.com>
2017-03-08 22:39:18 -0800

    Added some error checking

--- src/commands/weather_command.js
+++ src/commands/weather_command.js
@@ -1,10 +1,17 @@
 module.exports = (message, key) => {
 	let request = require('request');
 
-	let zip = message.content.slice(9);
-	console.log(zip);
+	//Validate user input
+	let zip = message.content.split(" ")[1];
+	if(zip.length != 5){
+		message.channel.sendMessage("Please enter 5 digits for the ZIP code");
+		return;
+	}
+
+	//Create API call URL
 	let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${key}`;
 
+	//Call API
 	request({
 		url: url,
 		json: true
@@ -15,8 +22,14 @@ module.exports = (message, key) => {
 				message.channel.sendMessage("Cannot find location");
 				return;
 			}
-			let tempF = (((body.main.temp - 273.15) * 1.8) + 32).toFixed(2);
-			message.channel.sendMessage(`The weather in ${body.name} is ${tempF} degrees Fahrenheit`);
+			if(body !== 'undefined'){
+				console.log(body);
+				let tempF = (((body.main.temp - 273.15) * 1.8) + 32).toFixed(2);
+				message.channel.sendMessage(`The weather in ${body.name} is ${tempF} degrees Fahrenheit`);
+			}
+			else {
+				message.channel.sendMessage("Cannot find location");
+			}
 			
 		}
 	);
