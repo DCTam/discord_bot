8ae5c8b2ddb26ce55d303e0af26e23f4616abe46
Daniel Tam <ctam.daniel@gmail.com>
2017-03-10 22:38:32 -0800

    Fixed documentations

--- src/commands/help_command.js
+++ src/commands/help_command.js
@@ -1,8 +1,8 @@
 module.exports = (message) => {
 	message.author.sendMessage('```' + 
-								'\n\n!roll - Roll a die once' +
-								'\n\n!weather <ZIP Code> - Input a 5 digit ZIP code to check weather' +
-								'\n\n!ison <Twitch Channel ID> - Check to see if streamer is on' + 
+								'\n\n!roll - Roll a die' +
+								'\n\n!weather <ZIP Code> - Input a 5 digit ZIP code to retreive weather information' +
+								'\n\n!ison <Twitch Channel ID> - Check to see if streamer is on and returns channel information' + 
 								'\n\n!rank <Battle Tag> - Input case-sensitive battle tag to retrieve rank information on Overwatch' +
 								'```');
 }
