5955775f1ddb822fc7fb9b12c4dc8b464b1794e5
Austin <wooaustin0@gmail.com>
2018-10-29 10:59:15 -0700

    Fixed indents and newlines

--- src/commands/fortnite_tracker.js
+++ src/commands/fortnite_tracker.js
@@ -12,26 +12,25 @@ module.exports = (message,key) => {
            'TRN-Api-Key': `${apiKey}`;
         }
     }),
-        function(error, res, body){
-            if(error){
-                message.channel.sendMessage('`Error retrieving account stats`');
-                console.log("It triggered an error");
-				return;
-            }
-            if(body.cod == 200){               
-                messsage.channel.sendMessage('```Javascript'+
-                                               'Username:'+ body.epicUserHandle+'\n'+
-                                               'TRN Rating(Elo): '+ body.stats.p9.trnRating.displayValue+ '\n'+ 
-                                               'Percentile: '+body.stats.p9.trnRating.percentile+'\n'+
-                                               'Squad Win Rate: '+body.stats.p9.winRatio.value+'\n'+
-                                               'Squad KDR: '+body.stats.p9.kd.value
-                                             );
-                console.log('Success');
-	       }
-           else {
-		          message.channel.sendMessage('`Your account doesnt exist or your input is incorrect`');
-                  console.log('Account non-existent');
-           }
-        
+    function(error, res, body){
+        if(error){
+            message.channel.sendMessage('`Error retrieving account stats`');
+            console.log("It triggered an error");
+            return;
+        }
+        if(body.cod == 200){               
+            messsage.channel.sendMessage('```Javascript'+
+                                            'Username:'+ body.epicUserHandle+'\n'+
+                                            'TRN Rating(Elo): '+ body.stats.p9.trnRating.displayValue+ '\n'+ 
+                                            'Percentile: '+body.stats.p9.trnRating.percentile+'\n'+
+                                            'Squad Win Rate: '+body.stats.p9.winRatio.value+'\n'+
+                                            'Squad KDR: '+body.stats.p9.kd.value
+                                         );
+            console.log('Success');
+	   }
+       else {
+		    message.channel.sendMessage('`Your account doesnt exist or your input is incorrect`');
+            console.log('Account non-existent');
+       }        
     }
 }
